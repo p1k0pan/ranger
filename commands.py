225195cca5b0f09b13dc63edee2845a6337f235f
p1k0pan <645758634@qq.com>
2021-09-01 12:50:35 +0800

    change to normal vim-key; activate extract function

--- commands.py
+++ commands.py
@@ -2,13 +2,13 @@ from ranger.api.commands import Command
 import os
 from ranger.core.loader import CommandLoader
 
-class paste_as_root(Command):
-	def execute(self):
-		if self.fm.do_cut:
-			self.fm.execute_console('shell sudo mv %c .')
-		else:
-			self.fm.execute_console('shell sudo cp -r %c .')
 
+class paste_as_root(Command):
+    def execute(self):
+        if self.fm.do_cut:
+            self.fm.execute_console('shell sudo mv %c .')
+        else:
+            self.fm.execute_console('shell sudo cp -r %c .')
 
 
 class mkcd(Command):
@@ -37,11 +37,12 @@ class mkcd(Command):
                 if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
                     self.fm.cd(s)
                 else:
-                    ## We force ranger to load content before calling `scout`.
+                    # We force ranger to load content before calling `scout`.
                     self.fm.thisdir.load_content(schedule=False)
                     self.fm.execute_console('scout -ae ^{}$'.format(s))
         else:
             self.fm.notify("file/directory exists!", bad=True)
+
 
 class fzf_select(Command):
     """
@@ -53,18 +54,20 @@ class fzf_select(Command):
 
     See: https://github.com/junegunn/fzf
     """
+
     def execute(self):
         import subprocess
         import os.path
         if self.quantifier:
             # match only directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         else:
             # match files and directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        fzf = self.fm.execute_command(
+            command, universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
@@ -72,9 +75,6 @@ class fzf_select(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
-
-
-
 
 class compress(Command):
     def execute(self):
@@ -106,33 +106,34 @@ class compress(Command):
         extension = ['.zip', '.tar.gz', '.rar', '.7z']
         return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
 
-class extracthere(Command):
+class extract_here(Command):
     def execute(self):
-        """ Extract copied files to current directory """
-        copied_files = tuple(self.fm.copy_buffer)
-
-        if not copied_files:
-            return
+        """ extract selected files to current directory."""
+        cwd = self.fm.thisdir
+        marked_files = tuple(cwd.get_selection())
 
         def refresh(_):
             cwd = self.fm.get_directory(original_path)
             cwd.load_content()
 
-        one_file = copied_files[0]
+        one_file = marked_files[0]
         cwd = self.fm.thisdir
         original_path = cwd.path
-        au_flags = ['-X', cwd.path]
+        au_flags = ['-x', cwd.path]
         au_flags += self.line.split()[1:]
         au_flags += ['-e']
 
         self.fm.copy_buffer.clear()
         self.fm.cut_buffer = False
-        if len(copied_files) == 1:
+        if len(marked_files) == 1:
             descr = "extracting: " + os.path.basename(one_file.path)
         else:
-            descr = "extracting files from: " + os.path.basename(one_file.dirname)
-        obj = CommandLoader(args=['aunpack'] + au_flags \
-                + [f.path for f in copied_files], descr=descr, read=True)
+            descr = "extracting files from: " + os.path.basename(
+                one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags
+                            + [f.path for f in marked_files], descr=descr,
+                            read=True)
 
         obj.signal_bind('after', refresh)
         self.fm.loader.add(obj)
+
