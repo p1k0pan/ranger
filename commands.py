c63c49942a38c98abab474c797a18ff9ccd5a25d
p1k0pan <645758634@qq.com>
2023-05-16 20:35:00 +0200

    ranger archives & open finder

--- commands.py
+++ commands.py
@@ -76,64 +76,64 @@ class fzf_select(Command):
             else:
                 self.fm.select_file(fzf_file)
 
-class compress(Command):
-    def execute(self):
-        """ Compress marked files to current directory """
-        cwd = self.fm.thisdir
-        marked_files = cwd.get_selection()
+# class compress(Command):
+#     def execute(self):
+#         """ Compress marked files to current directory """
+#         cwd = self.fm.thisdir
+#         marked_files = cwd.get_selection()
 
-        if not marked_files:
-            return
+#         if not marked_files:
+#             return
 
-        def refresh(_):
-            cwd = self.fm.get_directory(original_path)
-            cwd.load_content()
+#         def refresh(_):
+#             cwd = self.fm.get_directory(original_path)
+#             cwd.load_content()
 
-        original_path = cwd.path
-        parts = self.line.split()
-        au_flags = parts[1:]
+#         original_path = cwd.path
+#         parts = self.line.split()
+#         au_flags = parts[1:]
 
-        descr = "compressing files in: " + os.path.basename(parts[1])
-        obj = CommandLoader(args=['apack'] + au_flags + \
-                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
+#         descr = "compressing files in: " + os.path.basename(parts[1])
+#         obj = CommandLoader(args=['apack'] + au_flags + \
+#                 [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)
 
-        obj.signal_bind('after', refresh)
-        self.fm.loader.add(obj)
+#         obj.signal_bind('after', refresh)
+#         self.fm.loader.add(obj)
 
-    def tab(self, tabnum):
-        """ Complete with current folder name """
+#     def tab(self, tabnum):
+#         """ Complete with current folder name """
 
-        extension = ['.zip', '.tar.gz', '.rar', '.7z']
-        return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
+#         extension = ['.zip', '.tar.gz', '.rar', '.7z']
+#         return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
 
-class extract_here(Command):
-    def execute(self):
-        """ extract selected files to current directory."""
-        cwd = self.fm.thisdir
-        marked_files = tuple(cwd.get_selection())
+# class extract_here(Command):
+#     def execute(self):
+#         """ extract selected files to current directory."""
+#         cwd = self.fm.thisdir
+#         marked_files = tuple(cwd.get_selection())
 
-        def refresh(_):
-            cwd = self.fm.get_directory(original_path)
-            cwd.load_content()
+#         def refresh(_):
+#             cwd = self.fm.get_directory(original_path)
+#             cwd.load_content()
 
-        one_file = marked_files[0]
-        cwd = self.fm.thisdir
-        original_path = cwd.path
-        au_flags = ['-x', cwd.path]
-        au_flags += self.line.split()[1:]
-        au_flags += ['-e']
+#         one_file = marked_files[0]
+#         cwd = self.fm.thisdir
+#         original_path = cwd.path
+#         au_flags = ['-x', cwd.path]
+#         au_flags += self.line.split()[1:]
+#         au_flags += ['-e']
 
-        self.fm.copy_buffer.clear()
-        self.fm.cut_buffer = False
-        if len(marked_files) == 1:
-            descr = "extracting: " + os.path.basename(one_file.path)
-        else:
-            descr = "extracting files from: " + os.path.basename(
-                one_file.dirname)
-        obj = CommandLoader(args=['aunpack'] + au_flags
-                            + [f.path for f in marked_files], descr=descr,
-                            read=True)
+#         self.fm.copy_buffer.clear()
+#         self.fm.cut_buffer = False
+#         if len(marked_files) == 1:
+#             descr = "extracting: " + os.path.basename(one_file.path)
+#         else:
+#             descr = "extracting files from: " + os.path.basename(
+#                 one_file.dirname)
+#         obj = CommandLoader(args=['aunpack'] + au_flags
+#                             + [f.path for f in marked_files], descr=descr,
+#                             read=True)
 
-        obj.signal_bind('after', refresh)
-        self.fm.loader.add(obj)
+#         obj.signal_bind('after', refresh)
+#         self.fm.loader.add(obj)
 
